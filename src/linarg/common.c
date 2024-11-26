a7e66026a8fb36b096f34bf0c369ca517e3de926
Soham More <msoham2100@gmail.com>
2024-11-26 22:16:42 +0530

    fixed minor order bug in error_handler

--- src/linarg/common.c
+++ src/linarg/common.c
@@ -2,7 +2,7 @@
 
 void error_handler(const char* file, const char* function, size_t line_no)
 {
-    printf("in function %s, defined in file %s at line %zu:\n\t", file, function, line_no);
+    printf("in function %s, defined in file %s at line %zu:\n\t", function, file, line_no);
 }
 
 
